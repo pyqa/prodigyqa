f07a8c80770faaaceef4d19532d2f2ad2a71e844
Abhimaniu <abhimaniu.soni@gmail.com>
2018-12-11 16:21:57 +0530

    Update captcha.py
    Updated the code for handling return of captcha string.

--- core/captcha.py
+++ core/captcha.py
@@ -5,20 +5,14 @@ import pytesseract
 
 class Captcha:
 
-    def captcharead(self, imagepath=None):
+    def captchaRead(self, imagepath=None):
 
         if imagepath is None:
-            print('Please give Image path in sample_captcha.py file')
+            print('Please give Image path in the function defined in sample_captcha.py file')
             sys.exit(1)
 
-        # Read image path from command line
-        impath = imagepath
-
-        # Define config parameters.
-        # '-l eng'  for using the English language
-        # '--oem 1' for using LSTM OCR Engine
-        config = ('-l eng --oem 1 --psm 3')
-        im = cv2.imread(impath, cv2.IMREAD_COLOR)
-        # Run tesseract OCR on image
-        text = pytesseract.image_to_string(im, config=config)
-        return text
+        #Resampling the Image
+        check_output(['convert', imagepath, '-resample', '600', imagepath])
+        
+        #Returning the captcha text in the form of string.
+        return pytesseract.image_to_string(Image.open(imagepath))
