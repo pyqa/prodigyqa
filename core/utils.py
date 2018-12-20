91fba9442e31b9e422070c8f95a25217a0f2dedc
revanthmvs <revanth.mvs@imaginea.com>
2018-12-20 13:41:20 +0530

    captcha crack method added using teserract

--- core/utils.py
+++ core/utils.py
@@ -1,21 +1,14 @@
 """Image Comparison Module using Structural Similary and Open CV."""
 from subprocess import check_output
 from PIL import Image
-import sys
 import pytesseract
 
 
-class Captcha:
+class crackcaptcha:
 
-    def captcharead(self, imagepath=None):
+    def captcha_to_text(self, imagepath):
 
-        if imagepath is None:
-            print(
-                'Please give Image path in the function defined in sample_captcha.py file')
-            sys.exit(1)
+        return pytesseract.image_to_string(Image.open(imagepath))
 
         # Resampling the Image
-        check_output(['convert', imagepath, '-resample', '600', imagepath])
-
-        # Returning the captcha text in the form of string.
-        return pytesseract.image_to_string(Image.open(imagepath))
+        # check_output(['convert', imagepath, '-resample', '600', imagepath])
