63a17a87e32db0b2e52ac93c54c88b95a8de2685
Abhimaniu <abhimaniu.soni@gmail.com>
2018-12-24 18:24:50 +0530

    Update image_comparison_example.py

--- Examples/ImageCompare/image_comparison_example.py
+++ Examples/ImageCompare/image_comparison_example.py
@@ -1,27 +1,24 @@
-from imgqa.core.utils import ImageCompare
+from imgqa.core.comparison_util import CompareFiles
 import cv2
 
 # Variable Stack / Data
-img1 = cv2.imread("highway.jpg")
-img2 = cv2.imread("highway_altered.jpg")
-img3 = cv2.imread("sea.jpg")
+img1 = "highway.jpg"
+img2 = "highway_altered.jpg"
+img3 = "sea.jpg"
+excel1 = "first.xlsx"
+excel2 = "second.xlsx"
+json1 = "first.json"
+json2 = "second.json"
 
 
-class TestImageCompare(ImageCompare):
+class TestCompareFiles(CompareFiles):
     """Sample Test Suite."""
 
-    def test_compare_images(self):
-        # Considering the width and ensuring dimesion to be same
-        width = 2160
-        # keep original height
-        height = img2.shape[0]
-        dim = (width, height)
-        # resizing altered image2
-        resized_img2 = cv2.resize(img2, dim, interpolation=cv2.INTER_AREA)
-        height = 3840
-        dim = (width, height)
-        # resizing altered image3
-        resized_img3 = cv2.resize(img3, dim, interpolation=cv2.INTER_AREA)
-
-        self.grayscaling_images_and_comparing(img1, resized_img2)
-        # self.image_compare_thru_opencv(img1, resized_img2)
+    def test_compare_files(self):
+        # Comparing images
+        self.files_to_compare(img1, img2)
+        # Comparing excels
+        self.files_to_compare(excel1, excel2
+        # Comparing jsons
+        self.files_to_compare(json1, json2)
+        
