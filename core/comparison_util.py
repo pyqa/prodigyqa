be3aa4c38538e394f951eb8767c9495b85a0185f
Abhimaniu <abhimaniu.soni@gmail.com>
2018-12-24 17:53:08 +0530

    Update comparison_util.py
    Added comments and refactored the code.

--- core/comparison_util.py
+++ core/comparison_util.py
@@ -6,6 +6,8 @@ from skimage.measure import compare_ssim as ssim
 # import matplotlib.pyplot as plt
 import numpy as np
 import traceback
+from json import JSONDecodeError
+import os
 
 
 class ImageCompare:
@@ -135,38 +137,123 @@ class ImageCompare:
             traceback.print_exc()
 
 
-class CompareFiles(ImageCompare):
+class JsonCompare:
+
+    def compare_json(self, file1, file2, path=""):
+        try:
+            # Reading the jsons and converting them into dictionaries.
+            with open(file1) as first_json:
+                dict1 = json.load(first_json)
+            with open(file2) as second_json:
+                dict2 = json.load(second_json)
+
+            if path is "":
+                try:
+                    os.remove("/home/abhimanius/Desktop/json/json_diff.txt")
+                except OSError:
+                    pass
+            for k in dict1.keys():
+                # Checking whether some key present in one dictionary is not present in other dictionary.
+                if not k in dict2.keys():
+                    keydiff = (str(k) + " as key not in d2")
+                    with open('/home/abhimanius/Desktop/json/json_diff.txt', 'a') as the_file:
+                        the_file.write(str(keydiff))
+                else:
+                    if type(dict1[k]) is dict:
+                        if path == "":
+                            path = k
+                        else:
+                            path = path + "->" + k
+                        # Making recursive call by passing the keys which are present as dictionary object.
+                        self.compare_json(dict1[k], dict2[k], path)
+                    else:
+                        if dict1[k] != dict2[k]:
+                            keystr = (str(path), ":")
+                            first_file_val = " First file ", k, " : ", dict1[k]
+                            second_file_val = " Second file ", k, " : ", dict2[k]
+
+                            # Writing the difference to the file.
+                            with open('/home/abhimanius/Desktop/json/json_diff.txt', 'a') as the_file:
+                                the_file.write(str(keystr) + '\n')
+                                the_file.write(str(first_file_val) + '\n')
+                                the_file.write(str(second_file_val) + '\n')
+                                the_file.write('\n')
+        except JSONDecodeError:
+            print("Invalid json. Please provide file with proper json structure.")
+            quit()
+        except Exception:
+            traceback.print_exc()
+
+class ExcelCompare:
+
+    def compare_excel(self, file1, file2):
+        try:
+            # Reading the excel files
+            excel1 = pd.read_excel(file1)
+            excel2 = pd.read_excel(file2)
+
+            # Checking if the excels are empty
+            if excel1.empty == True and excel2.empty == True:
+                print("The excel files are empty")
+                quit()
+
+            # Checking whether the no.of rows is same in both excels.
+            elif len(excel1) != len(excel2):
+                print("The no. of rows in both excel is not same")
+                quit()
+
+            # Checking whether the no.of columns is same in both excels.
+            elif len(excel1.columns) == len(excel2.columns):
+                print("The no. of columns in both excel is not same")
+                quit()
+            else:
+                # Setting coulmn order same in both excels.
+                excel1.columns = excel2.columns
+
+                # Sorting excel data on the basis of a column.
+                excel1 = excel1.sort_values(
+                    'id', ascending=False).reset_index(inplace=False)
+                excel2 = excel2.sort_values(
+                    'id', ascending=False).reset_index(inplace=False)
+
+                #Getting the difference in data between both excels.
+                difference = excel1[excel1 != excel2]
+
+                # Writing the delta between both excels in a seperate excel file.
+                writer = pd.ExcelWriter(
+                    '../Examples/excel_diff.xlsx', engine='xlsxwriter')
+                difference.to_excel(writer, sheet_name='sheet1', index=False)
+        except Exception:
+            traceback.print_exc()
+
+
+
+
+class CompareFiles(ImageCompare, JsonCompare, ExcelCompare):
 
     def files_to_compare(self, file1, file2):
-        supported_formats = ('.xls', '.xlsx', '.png', '.jpeg', '.jpg')
+        supported_formats = ('.xls', '.xlsx', '.png', '.jpeg', '.jpg','.json')
+
+        # Checking whether the 2 input files belong to supported formats.
         if ''.join(pathlib.Path(file1).suffixes) not in supported_formats and \
                 ''.join(pathlib.Path(file2).suffixes) not in supported_formats:
             print("Not a supported file format.")
         elif ''.join(pathlib.Path(file1).suffixes) in ('.xls', '.xlsx') and\
                 ''.join(pathlib.Path(file2).suffixes) in ('.xls', '.xlsx'):
-            self._compare_excel(file1, file2)
+            ExcelCompare.compare_excel(file1, file2)
         elif ''.join(pathlib.Path(file1).suffixes) in ('.png', '.jpeg', '.jpg') and\
                 ''.join(pathlib.Path(file2).suffixes) in ('.png', '.jpeg', '.jpg'):
             img1 = cv2.imread(file1)
             img2 = cv2.imread(file2)
+
             # keep original height
             width = 2160
             height = img2.shape[0]
             dim = (width, height)
 
-            # resizing altered image
+            # resizing image2
             resized_img2 = cv2.resize(img2, dim, interpolation=cv2.INTER_AREA)
             ImageCompare.image_compare_thru_opencv(self, img1, resized_img2)
-
-    def _compare_excel(self, file1, file2):
-        excel1 = pd.read_excel(file1)
-        excel2 = pd.read_excel(file2)
-        excel1.columns = excel2.columns
-        excel1 = excel1.sort_values(
-            'id', ascending=False).reset_index(inplace=False)
-        excel2 = excel2.sort_values(
-            'id', ascending=False).reset_index(inplace=False)
-        difference = excel1[excel1 != excel2]
-        writer = pd.ExcelWriter(
-            '../Examples/excel_diff.xlsx', engine='xlsxwriter')
-        difference.to_excel(writer, sheet_name='sheet1', index=False)
+        elif ''.join(pathlib.Path(file1).suffixes) in ('.json') and \
+                        ''.join(pathlib.Path(file2).suffixes) in ('.json'):
+            JsonCompare.compare_json(file1, file2)
