3e4e42fddb3c3fa0f292c70e49be255f6064f5b1
revanthmvs <revanth.mvs@imaginea.com>
2018-12-26 16:45:25 +0530

    removed unused-duplicate files

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-"""This file could have imports once the core is developed fully."""
-from core.apitester import ApiTester  # NoQa
-from core.browseractions import BrowserActions
-from core.comparison_util import ImageCompare
-from core.comparison_util import CompareFiles
