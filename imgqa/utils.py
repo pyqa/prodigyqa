f6e2ee5b66f8b5f5d51c0b550e70c0e075718f44
revanthmvs <revanth.mvs@imaginea.com>
2018-12-27 15:12:00 +0530

    pycodestyle and flake8 fixes

--- imgqa/utils.py
+++ imgqa/utils.py
@@ -3,9 +3,9 @@ from PIL import Image
 import pytesseract
 
 
-class crackcaptcha:
-	"""Extract the text from image presented."""
+class CrackCaptcha:
+    """Extract the text from image presented."""
 
     def captcha_to_text(self, imagepath):
-    	"""Method to return extracted text from passed image"""
+        """Method to return extracted text from passed image."""
         return pytesseract.image_to_string(Image.open(imagepath))
