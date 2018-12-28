6bfb06fd9c3c9f0e5d9e204dec0055da67195200
revanthmvs <revanth.mvs@imaginea.com>
2018-12-28 20:13:10 +0530

    added few additional steps

--- Examples/sampleseleniumtest.py
+++ Examples/sampleseleniumtest.py
@@ -13,20 +13,31 @@ class Page:
     """hold all locators for POM style implementation."""
 
     base_url = "http://www.python.org"
-    search_box = {"locatorvalue": 'q',
-                  "by": "By.XPATH", "value": "selenium"}
+    two_url = "https://xkcd.com/979/"
+    search_box = {"locatorvalue": 'id-search-field',
+                  "by": "By.ID", "value": "selenium " + Keys.ENTER}
+    button = {"locatorvalue": '#topLeft > ul > li:nth-child(1) > a',
+              "by": "By.CSS_SELECTOR"}
 
 
 class TestClass(BrowserActions):
     """Test Class Container for test cases."""
 
-    def test_python_search(self):
-        """Sample case for navigating to python site and search selenium."""
-
+    def setUp(self):
         # Added the below line for travis to enable headless mode test
         self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        # pass
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_python_search(self):
+        """Sample case for navigating to python site and search selenium."""
         self.open(Page.base_url)
+        self.set_window_size(1200, 800)
         self.maximize()
         self.assertIn("Python", self.get_title())
-        self.send_keys(Page.search_box[locatorvalue], Page.search_box[value])
-        self.send_keys(Keys.ENTER)
+        self.click(Page.search_box)
+        self.send_keys(Page.search_box)
+        self.open(Page.two_url)
+        self.click(Page.button)
