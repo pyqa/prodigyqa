cfb6742a41134675464c56f8ad61b9494adb3dae
revanthmvs <revanth.mvs@imaginea.com>
2018-12-28 18:31:11 +0530

    updated a simple example with chrome headless run

--- Examples/sampleseleniumtest.py
+++ Examples/sampleseleniumtest.py
@@ -1,35 +1,32 @@
 """Sample test scripts for selenium pytest sample."""
 
 from imgqa import BrowserActions
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
 
 
-class Login:
+class Page:
     """hold all locators for POM style implementation."""
 
-    base_url = "https://example.com/"
-    login_btn = {"locatorvalue": '//*[@id="menu-item-15255"]/a',
-                 "by": "By.XPATH"}
-    username = {"locatorvalue": 'username',
-                "by": "By.ID", "value": "alpha"}
-    password = {"locatorvalue": 'password',
-                "by": "By.ID", "value": "alpha"}
-    login_button = {"locatorvalue": 'loginButton', "by": "By.ID"}
+    base_url = "http://www.python.org"
+    search_box = {"locatorvalue": 'q',
+                  "by": "By.XPATH", "value": "selenium"}
 
 
 class TestClass(BrowserActions):
     """Test Class Container for test cases."""
-    def login(self, username_dict=None,
-              password_dict=None,
-              login_button=None):
-        self.send_keys(username_dict)
-        self.send_keys(password_dict)
-        self.click(login_button)
 
-    def test_googlesearch1(self):
-        """Just to Open www.google.com and search a list."""
-        self.open(Login.base_url)
+    def test_python_search(self):
+        """Sample case for navigating to python site and search selenium."""
+
+        # Added the below line for travis to enable headless mode test
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.open(Page.base_url)
         self.maximize()
-        self.click(Login.login_btn)
-        self.login(username_dict=Login.username,
-                   password_dict=Login.password,
-                   login_button=Login.login_button)
+        self.assertIn("Python", self.get_title())
+        self.send_keys(Page.search_box[locatorvalue], Page.search_box[value])
+        self.send_keys(Keys.ENTER)
