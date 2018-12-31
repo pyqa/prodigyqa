913935eb6c1be6a430d0ddf265cec392679cde8d
Abhimaniu <abhimaniu.soni@imaginea.com>
2018-12-31 17:11:15 +0530

    Update image_comparison_example.py

--- Examples/ImageCompare/image_comparison_example.py
+++ Examples/ImageCompare/image_comparison_example.py
@@ -1,5 +1,5 @@
 """Example for Comparison module."""
-from imgqa import ImageCompare, JsonCompare, SpreadsheetCompare
+from imgqa import CompareFiles
 
 # Variable Stack / Data
 img1_path = "highway.jpg"
@@ -7,30 +7,23 @@ img2_path = "highway_altered.jpg"
 img3_path = "sea.jpg"
 excel1_path = "first.xlsx"
 excel2_path = "second.xlsx"
-json1_path = "first.json"
-json2_path = "second.json"
+json1_path = "third.json"
+json2_path = "fourth.json"
 
 
-class TestCompareImages(ImageCompare):
+class TestCompareFiles(CompareFiles):
     """Sample Test Suite."""
 
     def test_compare_images(self):
         """Compare images."""
-        self.grayscaling_and_comparing_images_thru_mse_ssim(img1_path,
-                                                            img2_path)
-
-
-class TestCompareJsons(JsonCompare):
-    """Sample Test Suite."""
-
+        self.compare_images(img1_path,
+                            img2_path)
+        
     def test_compare_jsons(self):
-        """Comparing images."""
+        """Comparing jsons."""
         self.compare_json(json1_path, json2_path)
-
-
-class TestCompareExcels(SpreadsheetCompare):
-    """Sample Test Suite."""
-
+        
     def test_compare_excels(self):
-        """Compare images."""
+        """Compare excels."""
         self.compare_excel(excel1_path, excel2_path)
+
