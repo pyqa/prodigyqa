1de467699c9c4885896381757ec61fa392aa01db
Abhimaniu <abhimaniu.soni@gmail.com>
2019-01-02 11:48:06 +0530

    Create files_comparison_example.py

---
+++ Examples/ImageCompare/files_comparison_example.py
@@ -0,0 +1,28 @@
+"""Example for Comparison module."""
+from imgqa import CompareFiles
+
+# Variable Stack / Data
+img1_path = "highway.jpg"
+img2_path = "highway_altered.jpg"
+img3_path = "sea.jpg"
+excel1_path = "first.xlsx"
+excel2_path = "second.xlsx"
+json1_path = "third.json"
+json2_path = "fourth.json"
+
+
+class TestCompareFiles(CompareFiles):
+    """Sample Test Suite."""
+
+    def test_compare_images(self):
+        """Compare images."""
+        self.compare_images(img1_path,
+                            img2_path)
+        
+    def test_compare_jsons(self):
+        """Comparing jsons."""
+        self.compare_json(json1_path, json2_path)
+        
+    def test_compare_excels(self):
+        """Compare excels."""
+        self.compare_excel(excel1_path, excel2_path)
