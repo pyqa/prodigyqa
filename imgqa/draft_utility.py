ab6e9ce7c8bed1c921204e6946d32f57e716784f
Obanna Komera <obanna.komera@imaginea.com>
2019-01-17 16:34:16 +0530

    added xls, xlsx, html, hdf, tsv, csv file comparison functions to draft utility

--- imgqa/draft_utility.py
+++ imgqa/draft_utility.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 """Comparison Module for Images, Files like CSV, Excel, PDF etc."""
-# import os
-# import sys
-# import unittest
-# import collections
-# import pandas as pd
 import unittest
 import cv2
 from skimage.measure import compare_ssim as ssim
@@ -15,26 +10,24 @@ import logging
 import pandas as pd
 
 
-class CompareFiles(unittest.TestCase):
+class Compare(unittest.TestCase):
     """File Comparison module which includes image, csv and workbook."""
 
-    def compare_images(self, source_img, target_img):
-        """Compare two images on the basis mse and ssim index.
+    def images(self, source, target):
+        """Compare images on the basis mse and ssim index.
 
-        :param source_img: source image.
-        :param target_img: target image.
-        :return: list of boolean value of image comparision,
-        mse value and ssim value.
+        :param source: source image.
+        :param target: target image.
+        :return: True/False.
+        :rtype: bool.
         """
-        # list of expected image extensions
-        extn = ('jpg', 'jpeg', "png")
-        if source_img.split(".") not in extn and\
-                target_img.split(".") not in extn:
-            logging.warning("Invalid image file extension")
+        image_extn = ('jpg', 'jpeg', "png")
+        if source.split(".") and target.split(".") not in image_extn:
+            logging.warning("Invalid image extension")
             return False
 
-        source = cv2.imread(source_img)
-        target = cv2.imread(target_img)
+        source = cv2.imread(source)
+        target = cv2.imread(target)
         if self.__compare_images_structure(source, target) \
             and self.__compare_image_mse(source, target) \
                 and self.__compare_image_ssim(source, target):
@@ -45,17 +38,17 @@ class CompareFiles(unittest.TestCase):
             self.__images_visual_difference(source, target)
             return False
 
-    def __compare_images_structure(self, source_image, target_image):
-        """Checkpoint 1 to measure on size and shape of image.
+    def __compare_images_structure(self, source, target):
+        """Checkpoint to measure on size and shape of image.
 
-        :param source_image: source image
-        :param target_image: target image
-        :return: returns boolean value based on images structure similarity
-        :rtype: boolean value.
+        :param source: source image.
+        :param target: target image.
+        :return: True/False.
+        :rtype: bool.
         """
-        if source_image.shape == target_image.shape:
+        if source.shape == target.shape:
             logging.info("Images of same size")
-            difference = cv2.subtract(source_image, target_image)
+            difference = cv2.subtract(source, target)
             b, g, r = cv2.split(difference)
             if cv2.countNonZero(b) == cv2.countNonZero(g) == cv2.countNonZero(r) == 0:
                 logging.info("The images are completely Equal")
@@ -68,67 +61,64 @@ class CompareFiles(unittest.TestCase):
             logging.warning("The images have different size and channels")
             return False
 
-    def __compare_images_mse(self, source_image, target_image):
+    def __compare_images_mse(self, source, target):
         """Checkpoint to measure Mean Square Error (MSE) difference of images.
 
-        :param source_image:
-        :param target_image:
-        :return: returns boolean value based on images mse values
-        :rtype: boolean value
+        :param source: Source image.
+        :param target: Target Image.
+        :return: True/False.
+        :rtype: bool.
 
         """
-        mse_val = np.sum((source_image.astype("float") - target_image.astype("float")) ** 2)
-        mse_val /= float(source_image.shape[0] * source_image.shape[1])
+        mse_val = np.sum((source.astype("float") - target.astype("float")) ** 2)
+        mse_val /= float(source.shape[0] * source.shape[1])
 
         if mse_val == 0.00:
-            logging.info("Source and target images MSE Value are same")
+            logging.info("Source and target images have same MSE value")
             return True
         else:
-            logging.warning("Source and target images\
-            MSE Value: {0}" % mse_val)
+            logging.warning("Source and target images have different MSE Value: {0}" % mse_val)
             return False
 
-    def __compare_images_ssim(self, source_image, target_image):
+    def __compare_images_ssim(self, source, target):
         """Checkpoint to measure Structural Similarity Index (SSIM) difference of images.
 
-        :param source_image:
-        :param target_image:
-        :return: returns boolean value based on images ssim values
-        :rtype: boolean value
+        :param source: Source Image.
+        :param target: Target Image.
+        :return: True/False.
+        :rtype: bool.
         """
-        ssim_val = ssim(source_image, target_image)
+        ssim_val = ssim(source, target)
         if ssim_val == 1.00:
-            logging.info("Source and target images SSIM Value sre same:")
+            logging.info("Source and target images have same SSIM Value")
             return True
         else:
-            logging.warning("Source and target images\
-            SSIM Value: {0}" % ssim_val)
+            logging.warning("Source and target images have different SSIM value: {0}" % ssim_val)
             return ssim_val
 
-    def __images_visual_difference(self, source_image, target_image):
+    def __images_visual_difference(self, source, target):
         """"Show the images visual difference.
 
-        :param source_image:
-        :param target_image:
+        :param source: Source image.
+        :param target: Target image.
         :return: NA
         :rtype: NA
         """
-        difference = cv2.subtract(source_image, target_image)
+        difference = cv2.subtract(source, target)
         cv2.imshow("difference", difference)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def compare_json(self, source, target):
-        """Compare json files and returns text file with the difference.
+    def json(self, source, target):
+        """Compare json files.
 
         :param source: source json file Path.
         :param target: target json path.
-        :return: True/False.
-        :rtype: boolean
+        :return: NA.
+        :rtype: NA.
         """
         try:
-            if source.split(".")[1] != 'json' and\
-                    target.split(".")[1] != 'json':
+            if source.split(".")[1] and target.split(".")[1] != 'json':
                 logging.warning("File extensions are not valid.")
                 return False
             else:
@@ -144,13 +134,12 @@ class CompareFiles(unittest.TestCase):
     def __compare_dictionaries(self, source, target, source_name, target_name, path=""):
         """Compare two dictionaries recursively to find non matching elements.
 
-        Args:
-            :param source: source dictionary
-            :param target: target dictionary
-            :param source_name: source dictionary name
-            :param target_name: target dictionary name
-
-        return:
+        :param source: source dictionary.
+        :param target: target dictionary.
+        :param source_name: source dictionary name.
+        :param target_name: target dictionary name.
+        :return: non matching elements of two dictionaries.
+        :rtype: str.
         """
         err = ''
         key_err = ''
@@ -175,36 +164,117 @@ class CompareFiles(unittest.TestCase):
 
         return key_err + value_err + err
 
-    def compare_spreadsheet(self, source, target):
-        """Compare two spreadsheets and generates an excel file containing the.
+    def files(self, source, target):
+        """Compare two files of xls or xlsx or html or hdf or csv or tsv and return difference.
 
-        difference. If resultant excel file contain empty cell that means
-        the value is same in both excels else not.
         :param source: Source file Path.
         :param target: Target file path.
-        :return: data that differs in source and target files.
-        :rtype: data frame
+        :return: True/False.
+        :rtype: bool.
         """
         file_extn = ('xls', 'xlsx', 'csv', 'tsv', 'hdf', 'html')
-        if (source.split(".")[1] and target.split(".")[1]) in file_extn:
-            source_data = self.__load_into_dataframe(source)
-            target_data = self.__load_into_dataframe(target)
-            if source_data.split('.')[1] and target_data.split('.')[1] in ('xls','xlsx'):
-                # Validate length of work sheets
-                # For every work sheet, iterate through the data
-                # pd.ExcelFile().sheet_names
-                pass
 
+        if (source.split(".")[1] and target.split(".")[1]) in file_extn:
+            if (source.split(".")[1] and target.split(".")[1]) in ('xls', 'xlsx'):
+                return self.__compare_workbooks(source, target)
+            elif source.split(".")[1] in ('xls', 'xlsx') and target.split(".")[1] not in ('xls', 'xlsx'):
+                return self.__compare_spreadsheet_and_non_spreadsheet(source, target)
+                pass
+            elif source.split(".")[1] not in ('xls', 'xlsx') and target.split(".")[1] in ('xls', 'xlsx'):
+                return self.__compare_spreadsheet_and_non_spreadsheet(source, target)
+                pass
+            elif (source.split(".")[1] and target.split(".")[1]) not in ('xls', 'xlsx'):
+                return self.__compare_non_workbook_files(source, target)
         else:
             logging.warning("File extensions are not valid.")
             return False
 
-        return source_data[source_data != target_data]
+    def __compare_workbooks(self, source, target):
+        """Compare two xls or xlsx files and return difference and boolean.
+
+        :param source: Source file Path.
+        :param target: Target file path.
+        :return: True/False.
+        :rtype: bool.
+        """
+        source_dataframe = pd.ExcelFile(source)
+        target_dataframe = pd.ExcelFile(target)
+        flag = 0
+        if source_dataframe.sheet_names == target_dataframe.sheet_names:
+            source_sheets = source_dataframe.sheet_names
+            for source_sheet in source_sheets:
+                if source_dataframe.parse(source_sheet) == target_dataframe.parse(source_sheet):
+                    logging.info("Spreadsheet '{0}' of " /
+                                 "source '{1}' and target '{2}' have same data".format /
+                                 (source_sheet, source.split(".")[0], target.split(".")[0]))
+                else:
+                    difference = source_dataframe.parse(source_sheet)[source_dataframe.parse(source_sheet) != target_dataframe.parse(source_sheet)]
+                    logging.warning("Spreadsheet '{0}' of " /
+                                    "source '{1}' and target '{2}' have different data\n {3}".format /
+                                    (source_sheet, source.split(".")[0], target.split(".")[0], difference))
+                    flag += 1
+            if flag == 0:
+                logging.info("Both source '{0}' and target '{1}' work" /
+                             "books have same data".format(source_sheet, source.split(".")[0], target.split(".")[0]))
+                return True
+            else:
+                logging.info("one or more spread sheets of source '{0}' and target '{1}'" /
+                             "have different data".format(source_sheet, source.split(".")[0], target.split(".")[0]))
+                return False
+
+    def __compare_non_workbook_files(self, source, target):
+        """Compare two html or hdf or csv or tsv and return difference and boolean.
+
+        :param source: Source file Path.
+        :param target: Target file path.
+        :return: True/False.
+        :rtype: bool.
+        """
+        source_data = self.__load_into_dataframe(source)
+        target_data = self.__load_into_dataframe(target)
+        difference = source_data[source_data != target_data]
+        if difference == '':
+            logging.info("Both source '{0}' and target '{1}'" /
+                         "have same data".format(source.split(".")[0], target.split(".")[0]))
+            return True
+        else:
+            logging.warning("Source '{0}' and target '{1}'" /
+                            "have different data\n {2}".format(source.split(".")[0], target.split(".")[0], difference))
+            return False
+
+    def __compare_spreadsheet_and_non_spreadsheet(self, source, target):
+        """Compare two files of xls or xlsx and html or hdf or csv or tsv and return difference and boolean.
+
+        :param source: Source file Path.
+        :param target: Target file path.
+        :return: True/False.
+        :rtype: bool.
+        """
+        if source.split(".")[1] in ('xls', 'xlsx') and target.split(".")[1] not in ('xls', 'xlsx'):
+            df = pd.ExcelFile(source)
+            data = self.__load_into_dataframe(target)
+        elif source.split(".") not in ('xls', 'xlsx')[1] and target.split(".")[1] in ('xls', 'xlsx'):
+            df = pd.ExcelFile(target)
+            data = self.__load_into_dataframe(source)
+        for sheet in df.sheet_names:
+            if df.parse(sheet) in data:
+                logging.info("Both source '{0}' and target '{1}'" /
+                             "have same data".format(source.split(".")[0], target.split(".")[0]))
+                break
+        else:
+            logging.warning("source '{0}' and target '{1}'" /
+                            "have different data".format(source.split(".")[0], target.split(".")[0]))
+            return False
+        return True
 
     def __load_into_dataframe(self, source):
-        if source.split(".")[1] in ('xls', 'xlsx'):
-            return self.__read_speadsheet(source)
-        elif source.split(".")[1] == 'csv':
+        """Load hdf or csv or tsv file and return data.
+
+        :param source: source file path
+        :return: file data.
+        :rtype: data frame.
+        """
+        if source.split(".")[1] == 'csv':
             return self.__read_csv(source)
         elif source.split(".")[1] == 'hdf':
             return self.__read_hdf(source)
@@ -213,18 +283,38 @@ class CompareFiles(unittest.TestCase):
         elif source.split(".")[1] == 'tsv':
             return self.__read_tsv(source)
 
-    def __read_speadsheet(self, sheet):
-        return pd.read_excel(sheet)
+    def __read_csv(self, source, sep=None):
+        """Load csv file and return data.
 
-    def __read_csv(self, csv, sep=None):
-        return pd.read_csv(csv)
+        :param source: source file path
+        :return: file data.
+        :rtype: data frame.
+        """
+        return pd.read_csv(source)
 
-    def __read_hdf(self, hdf):
-        return pd.read_hdf(hdf)
+    def __read_hdf(self, source):
+        """Load hdf file and return data.
 
-    def __read_html(self, html):
-        return pd.read_html(html)
+        :param source: source file path
+        :return: file data.
+        :rtype: data frame.
+        """
+        return pd.read_hdf(source)
 
-    def __read_tsv(self, tsv, sep='\t'):
-        return pd.read_csv(tsv, sep=sep)
+    def __read_html(self, source):
+        """Load html file and return data.
 
+        :param source: source file path
+        :return: file data.
+        :rtype: data frame.
+        """
+        return pd.read_html(source)
+
+    def __read_tsv(self, source, sep='\t'):
+        """Load tsv file and return data.
+
+        :param source: source file path
+        :return: file data.
+        :rtype: data frame.
+        """
+        return pd.read_csv(source, sep=sep)
