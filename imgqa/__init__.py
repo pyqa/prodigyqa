e2f02d6b0df8d65e5bcb18bcf9056a49afa84ac7
revanthmvs <revanth.mvs@imaginea.com>
2019-01-23 14:08:43 +0530

    fix for comparison import

--- imgqa/__init__.py
+++ imgqa/__init__.py
@@ -1,4 +1,4 @@
 """This file could have imports once the core is developed fully."""
 from imgqa.apitester import ApiTester  # noqa
 from imgqa.browseractions import BrowserActions  # noqa
-from imgqa.draft_utility import Compare  # noqa
+from imgqa.comparison import Compare  # noqa
