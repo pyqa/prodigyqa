9144ea22456b40dda0d37fc5fb70ddbc457f5d05
Revant <revanth.mvs@imaginea.com>
2019-01-30 14:39:58 +0530

    bump version to 0.3.2 for flake8 change
    bump version to 0.3.2 for flake8 change

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except IOError:
 
 setup(
     name='imgqa',
-    version='0.3.1',
+    version='0.3.2',
     description='Test Automation Framework',
     long_description=long_description,
     long_description_content_type='text/markdown',
