9c67dacc2e386a8897bfe9d51a84639f612d146e
Obanna Komera <obanna.komera@imaginea.com>
2019-02-01 16:22:27 +0530

    fixed flake8 error #14

--- Examples/FilesCompare/samplecomparisontest.py
+++ Examples/FilesCompare/samplecomparisontest.py
@@ -8,8 +8,12 @@ source_xl = "source.xlsx"
 target_xl = "target.xlsx"
 source_csv = "source_csv.csv"
 target_csv = "target_csv.csv"
-source_json = "{'as': 1, 'a': {'b': {'cs':10, 'qqq': {'qwe':1}}, 'd': {'csd':30}}}"
-target_json = "{'as': 1, 'a': {'b': {'ds':10, 'qqq': {'qwe':11}}, 'd': {'dsd':40}}}"
+source_json = "{'as': 1," \
+              "'a': {'b': {'cs':10, 'qqq': {'qwe':1}}," \
+              "'d': {'csd':30}}}"
+target_json = "{'as': 1," \
+              "'a': {'b': {'ds':10, 'qqq': {'qwe':11}}," \
+              "'d': {'dsd':40}}}"
 
 
 class TestCompareFiles(Compare):
@@ -26,13 +30,13 @@ class TestCompareFiles(Compare):
     def test_compare_workbooks(self):
         """Compare spreadsheet.
 
-        if files type is xl, xl file will be generated with file difference.
+        xl file will be generated with file difference.
         """
         self.compare_files(source_xl, target_xl)
 
     def test_compare_files(self):
         """Compare spreadsheet.
 
-        if files type is xl and non xl, xl file will be generated with file difference.
+        xl file will be generated with file difference.
         """
         self.compare_files(source_csv, target_csv)
