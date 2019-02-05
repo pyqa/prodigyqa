9860b9a31b607a86d6b0d05ea457e123615fe1cf
Revant <revanth.mvs@imaginea.com>
2019-02-05 17:34:34 +0530

    Version bump of imgqa
    Version bump of imgqa

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except IOError:
 
 setup(
     name='imgqa',
-    version='0.4.0',
+    version='0.5.0',
     description='Test Automation Framework',
     long_description=long_description,
     long_description_content_type='text/markdown',
