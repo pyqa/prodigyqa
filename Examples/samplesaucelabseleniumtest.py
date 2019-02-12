0116c33f0243f191063053835ef775b03a851929
Abhimaniu <abhimaniu.soni@gmail.com>
2019-02-12 14:26:22 +0530

    Update samplesaucelabseleniumtest.py
    Modified code for running sequential test cases in same browser session.

--- Examples/samplesaucelabseleniumtest.py
+++ Examples/samplesaucelabseleniumtest.py
@@ -1,56 +1,74 @@
 """Sample test scripts for saucelab pytest sample."""
 
-from imgqa import BrowserActions
-from selenium.webdriver.chrome.options import Options
+import pytest
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
-
-# Selenium 3.14+ doesn't enable certificate checking                
-import urllib3                                                      
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) 
-
-chrome_options = Options()
-chrome_options.add_argument("--headless")
+from imgqa import BrowserActions
 
 
 class Page:
     """hold all locators for POM style implementation."""
 
     base_url = "http://www.python.org"
+    fb_base_url = "http://www.facebook.com"
     two_url = "https://xkcd.com/979/"
     search_box = {"locatorvalue": 'id-search-field',
                   "by": "By.ID", "value": "selenium " + Keys.ENTER}
     button = {"locatorvalue": '#topLeft > ul > li:nth-child(1) > a',
               "by": "By.CSS_SELECTOR"}
-    desired_cap = {                   
-    'platform': "Mac OS X 10.12", 
-    'browserName': "chrome",      
-    'version': "latest",          
-    }   
-    # Enter your sauce lab access credentials below.
-    username = ''                                  
-    access_key = ''  
+    # Use your sauce lab credentials.
+    username = ''
+    access_key = ''
+    desired_cap = {
+        'platform': "Mac OS X 10.12",
+        'browserName': "chrome",
+        'version': "latest",
+    }
 
 
-class TestClass(BrowserActions):
-    """Test Class Container for test cases."""
+@pytest.fixture(scope="class")
+def setup_teardown_fixture(request):
+    """Fixture for class level setup and teardown methods.
 
-    def setUp(self):
-        # Added the below line for configuring remote web driver.
-        self.driver = webdriver.Remote(                                                                      
-                command_executor='https://{}:{}@ondemand.saucelabs.com/wd/hub'.format(Page.username, Page.access_key), 
-                desired_capabilities=Page.desired_cap)                                                            
+    :param request: request for a particular functionality.
+    :return: NA
+    :rtype: NA
+    """
 
-    def tearDown(self):
-        self.driver.quit()
+    class Setup_Teardown(BrowserActions):
+        def __init__(self):
+            """Configuring remote web driver.
+
+            :param: NA
+            :return: NA
+            :rtype: NA
+            """
+            self.driver = webdriver.Remote(
+                command_executor='https://{}:{}@ondemand.saucelabs.'
+                                 'com/wd/hub'.format(Page.username,
+                                                     Page.access_key),
+                desired_capabilities=Page.desired_cap)
+
+    request.cls.st = Setup_Teardown()
+    yield
+    request.cls.st.driver.quit()
+
+
+@pytest.mark.usefixtures("setup_teardown_fixture")
+class TestClass():
 
     def test_python_search(self):
-        """Sample case for navigating to python site and search selenium."""
-        self.open(Page.base_url)
-        self.set_window_size(1200, 800)
-        self.maximize()
-        self.assertIn("Python", self.get_title())
-        self.click(Page.search_box)
-        self.send_keys(Page.search_box)
-        self.open(Page.two_url)
-        self.click(Page.button)
+        self.st.open(Page.base_url)
+        self.st.set_window_size(1200, 800)
+        self.st.maximize()
+        self.st.assertIn("Python", self.st.get_title())
+        self.st.click(Page.search_box)
+        self.st.send_keys(Page.search_box)
+        self.st.open(Page.two_url)
+        self.st.click(Page.button)
+
+    def test_facebook_search(self):
+        self.st.open(Page.fb_base_url)
+        self.st.set_window_size(1200, 800)
+        self.st.maximize()
+        self.st.assertIn("Facebook", self.st.get_title())
