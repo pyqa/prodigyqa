a8bfc06bba4d4fcad28d16e864b9fdbdb61e8da4
Obanna Komera <obanna.komera@imaginea.com>
2019-02-14 15:57:38 +0530

    spell check WIP updates #5

--- imgqa/utils.py
+++ imgqa/utils.py
@@ -3,9 +3,12 @@ from PIL import Image
 import pytesseract
 
 
-class CrackCaptcha:
+class utilities:
     """Extract the text from image presented."""
 
     def captcha_to_text(self, imagepath):
         """Method to return extracted text from passed image."""
         return pytesseract.image_to_string(Image.open(imagepath))
+
+    def spellchecker(self):
+        pass
