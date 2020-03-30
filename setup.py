52912fee093e5168564a708e920b1e939ccfb1b3
Menta Venkata Surendra Revanth <revanth.menta@phenompeople.com>
2020-03-31 00:19:50 +0530

    MAINT:version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except IOError:
 
 setup(
     name='prodigyqa',
-    version='1.2.2',
+    version='1.3.0',
     description='Test Automation Framework',
     long_description=long_description,
     long_description_content_type='text/markdown',
